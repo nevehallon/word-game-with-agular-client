58650d39ed9ab1808596402b5a6b495f90b0c4d9
Neveh Allon <NevehAllon@gmail.com>
2021-01-13 19:26:24 +0200

    add base components

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ContainerComponent } from './components/container/container.component';
+import { FrameComponent } from './components/frame/frame.component';
+import { BoardComponent } from './components/board/board.component';
+import { ActionBarComponent } from './components/action-bar/action-bar.component';
+import { RackFrameComponent } from './components/rack-frame/rack-frame.component';
+import { RackComponent } from './components/rack/rack.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContainerComponent,
+    FrameComponent,
+    BoardComponent,
+    ActionBarComponent,
+    RackFrameComponent,
+    RackComponent
   ],
   imports: [
     BrowserModule,
---
+++ client/src/app/components/rack-frame/rack-frame.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-rack-frame',
+  templateUrl: './rack-frame.component.html',
+  styleUrls: ['./rack-frame.component.scss']
+})
+export class RackFrameComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
