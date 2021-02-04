b968fbfc94e9566c48a8b52e3ea52ce7cf4e39b3
Neveh Allon <34620850+nevehallon@users.noreply.github.com>
2021-02-04 17:22:02 +0200

    working with new definition source

--- client/src/app/interfaces/history-entry.ts
+++ client/src/app/interfaces/history-entry.ts
@@ -1,7 +1,3 @@
-export interface Definition {
-  word?: DefinitionElement;
-}
-
 export interface Score {
   computerScore?: number | string;
   playerScore?: number | string;
@@ -10,7 +6,7 @@ export interface Score {
 export interface HistoryEntry {
   isAI?: boolean;
   word?: string;
-  definitions?: Definition[];
+  definitions?: State[];
   points?: number | string;
   score?: Score;
   skip?:
@@ -20,18 +16,60 @@ export interface HistoryEntry {
       };
 }
 
-export interface DefinitionElement {
-  [key: string]: {
-    definition?: string[];
-    partOfSpeech?: string[];
-    synonyms?: string[];
-    typeOf?: string[];
-    hasTypes?: string[];
-    examples?: string[];
-    partOf?: string[];
-    derivation?: string[];
-    verbGroup?: string[];
-    also?: string[];
-    inCategory?: string[];
-  };
+export interface Definition {
+  id: number;
+  dic: number;
+  part: string;
+  txt: string;
+  ex: string[];
+  upvotes: number;
+  downvotes: number;
+}
+
+export interface FilteredDefinition {
+  id: number;
+  dic: number;
+  part: string;
+  txt: string;
+  ex: string[];
+  upvotes: number;
+  downvotes: number;
+}
+
+export interface Source {
+  default: number[];
+  active: any[];
+}
+
+export interface Po {
+  default: string[];
+  active: any[];
+}
+
+export interface Category {
+  default: string[];
+  active: any[];
+}
+
+export interface Filter {
+  source: Source;
+  pos: Po;
+  category: Category;
+}
+
+export interface Origin {
+  txt: string;
+  link: string[];
+}
+
+export interface State {
+  headword: string;
+  definitions: Definition[];
+  filteredDefinitions: FilteredDefinition[];
+  audio: string;
+  pronunciation: string;
+  isFilterPanelOpened: boolean;
+  filters: Filter;
+  origin: Origin;
+  userDevice?: any;
 }
